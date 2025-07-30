eaa1a7d3d0e39fed77c0c50065ab386f08ce04e6
bozon11 <bozonhigsa1@gmail.com>
2025-07-30 11:12:06 +0200

    Add hw-3 task-1, task-2, task-3

--- homework-solutions/js/hw-4/task-2.js
+++ homework-solutions/js/hw-4/task-2.js
@@ -14,3 +14,24 @@
   5. Проверить задание со следующими значениями в переменной age: 10, 17, 18, 19, 59, 60, 61
 
   */
+
+  const minAge = 18;
+  const maxAge = 60;
+  let age;
+
+  function ageChecker(age){
+    if(typeof age !== 'number'|| isNaN(age)){
+    console.log("Incorrect data type");
+  }else if (age < minAge){
+    console.log("You don't have access cause your age is " + age + " It's less then " + minAge)
+  }else if (age >= minAge && age < maxAge){
+    console.log("Welcome!");
+  }else if (age > maxAge){
+    console.log("Keep calm and look Culture channel");
+  }else {
+    console.log("Technical work");
+  }
+}
+ 
+
+ageChecker(61);
