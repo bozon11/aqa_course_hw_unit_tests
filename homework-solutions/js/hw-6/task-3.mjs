5b1cb7e62be5c1e117a35bab0a5d900200f7ce25
bozon11 <bozonhigsa1@gmail.com>
2025-08-07 16:45:29 +0200

    hw-6-3

--- homework-solutions/js/hw-6/task-3.mjs
+++ homework-solutions/js/hw-6/task-3.mjs
@@ -6,6 +6,14 @@
 
   Присвойте результат в переменную "unique"
 */
-let unique;
+let unique = [];
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 4, 6, 8, 10, 1, 3, 5, 7, 9];
+
+for (const item of number) {
+  if (!unique.includes(item)) {
+    unique.push(item);
+  }
+}
+console.log(unique);
 
 export { unique };
