474d3118cc21ca7c954c6876c24ad56a9d590390
bozon11 <bozonhigsa1@gmail.com>
2025-08-11 21:07:37 +0200

    Add task-1 and task 3 + adjustments "set" in package.json

--- homework-solutions/js/hw-7/task-1.mjs
+++ homework-solutions/js/hw-7/task-1.mjs
@@ -5,9 +5,15 @@
   - Например: mergeArrays([1,2], [3,4], [5,6]) // [1,2,3,4,5,6]
   - Решить с использованием Spread operator
 */
-function mergeArrays() {
-  // Ваш код
+function mergeArrays(...arr) {
+  let sum = [];
+  for (const item of arr) {
+    sum = sum.concat(item);
+  }
+  return sum;
 }
+let result = mergeArrays([1, 2], [3, 4], [5, 6]);
+console.log(result);
 /*
   2. Devide by _
     - Написать функцию, которая преобразует любое предложение в вот_Такой_Вот_Вид и возвращает его. 
@@ -15,8 +21,20 @@ function mergeArrays() {
     - Пример: I am super engineer => i_Am_Super_Engineer
   */
 function devideBy(sentence) {
-  // Ваш код
+  let separatedSentence = sentence.trim().split(/\s+/);
+  let newSentence = [];
+
+  for (const word of separatedSentence) {
+    if (word !== separatedSentence.at(0)) {
+      newSentence.push(word[0].toUpperCase() + word.slice(1).toLowerCase());
+    } else {
+      newSentence.push(word.toLowerCase());
+    }
+  }
+  return newSentence.join('_');
 }
+let a = devideBy('I    am    an     engineer');
+console.log(a);
 /*
   3. Фибаначчи
     - Напишите функцию fibonacci(n), возвращающую энное число Фибоначчи
@@ -25,8 +43,11 @@ function devideBy(sentence) {
       является суммой двух предыдущих
     - Например fibonacci(8) //21
   */
+
 function fibonacci(n) {
-  // Ваш код
+  return n < 1 ? 0 : n <= 2 ? 1 : fibonacci(n - 1) + fibonacci(n - 2);
 }
+
+console.log(fibonacci(2));
 
 export { mergeArrays, fibonacci, devideBy };
