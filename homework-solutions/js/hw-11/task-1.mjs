33d2a465f16d744b9c804ca1f822f208643eded2
bozon11 <bozonhigsa1@gmail.com>
2025-09-04 02:25:43 +0200

    hw-11-set. tast 1/2/3

--- homework-solutions/js/hw-11/task-1.mjs
+++ homework-solutions/js/hw-11/task-1.mjs
@@ -1,9 +1,99 @@
 class Employee {
-  // Ваш код
+  #salary;
+  constructor(firstName, lastName, profession, salary) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.profession = profession;
+    this.#salary = salary;
+  }
+
+  get firstName() {
+    return this._firstName;
+  }
+  set firstName(value) {
+    if (typeof value !== 'string') throw new Error('Invalid value');
+    this._firstName = value;
+  }
+
+  get lastName() {
+    return this._lastName;
+  }
+  set lastName(value) {
+    if (typeof value !== 'string') throw new Error('Invalid value');
+    this._lastName = value;
+  }
+  get profession() {
+    return this._profession;
+  }
+  set profession(value) {
+    if (typeof value !== 'string') throw new Error('Invalid value');
+    this._profession = value;
+  }
+  get salary() {
+    return this.#salary;
+  }
+  set salary(value) {
+    if (typeof value !== 'number' || value < 0) {
+      throw new Error("Emloyee's invoice has an error");
+    }
+    this.#salary = value;
+  }
+
+  getFullName() {
+    return `${this._firstName} ${this._lastName}`;
+  }
 }
+
+// const newEmployee = new Employee('Alex', 'Bird', 'qa', 5000);
+// const emp1 = new Employee('John', 'Doe', 'Developer', 3000);
+// console.log(emp1.firstName); // "John"
+// emp1.salary = 3100;
+// console.log(typeof emp1.salary);
+// console.log(newEmployee);
 
 class Company {
-  // Ваш код
+  #employees;
+  constructor(title, phone, address, employees = []) {
+    this._title = title;
+    this._phone = phone;
+    this._address = address;
+    this.#employees = employees;
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  get phone() {
+    return this._phone;
+  }
+
+  get address() {
+    return this._address;
+  }
+
+  addEmployee(employee) {
+    if (employee instanceof Employee) {
+      this.#employees.push(employee);
+      return true;
+    }
+    throw new Error('Invalid Employee');
+  }
+
+  getEmployees() {
+    return [...this.#employees];
+  }
+
+  getInfo() {
+    return `Компания: ${this.title}\nАдрес: ${this.address}\nКоличество сотрудников: ${this.#employees.length}`;
+  }
 }
 
+// const company = new Company('Tech Corp', 123456, 'Main Street');
+// const emp1 = new Employee('John', 'Doe', 'Developer', 3000);
+// const emp2 = new Employee('Barbara', 'Johnson', 'QA', 2500);
+// company.addEmployee(emp1);
+// company.addEmployee(emp2);
+// console.log(company.getEmployees());
+// console.log(company.getInfo());
 export { Employee, Company };
